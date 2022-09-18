a18220af0897b79ff9ad864b91376878065fdca6
Omer Evron <omerevron011@gmail.com>
2022-09-18 11:46:38 +0300

    first commit

---
+++ JS/1.1/variables.js
@@ -0,0 +1,17 @@
+let str = 'Hello World'
+let num = 5
+let randomObj = Object ('spaceship')
+
+let tastyCheese = true
+let empty = null
+let whoIsGod
+let symbol = Symbol ('Top')
+
+console.log(str);
+console.log(num);
+console.log(randomObj);
+console.log(tastyCheese);
+console.log(empty);
+console.log(whoIsGod);
+console.log(symbol);
+
