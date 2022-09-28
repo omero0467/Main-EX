f62a5b2e80c6b110fc6c65b886d8c24412c95194
Omer Evron <omerevron011@gmail.com>
2022-09-28 11:35:42 +0300

    com

---
+++ JS/9.3/app.js
@@ -0,0 +1,19 @@
+constfood = ["Noodle","Pasta","Ice-cream","Meat","Cucumber","Olives"];
+constfood1 = ["Fries","Ice-cream","Pizza","Olives","Hamburgers"];
+
+const compare = (a,b) => {
+    let myArr = []
+    for(let i=0; i<a.length; i++){
+        for (let j=0; j<b.length; j++) {
+            if (b[j]===a[i]) {
+                myArr.push(a[i])
+            }
+        }
+    }
+    if (myArr === []) {console.log(false);} else {
+
+        console.log(myArr);
+    }
+}
+
+compare(constfood1,constfood);
