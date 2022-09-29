19cd3f7993b8b1eac456db1c8397ff214c03a383
Omer Evron <omerevron011@gmail.com>
2022-09-29 10:59:41 +0300

    com.

--- JS/10.1/app.js
+++ JS/10.1/app.js
@@ -12,9 +12,13 @@ isString()
 const firstWordUpperCase = (a, func) => {
     let arr = a.split(' ');
     arr[0] = arr[0].toUpperCase();
-    func = (x) => {
-        arr = x.join('-');}
-    func(arr),
-    console.log(arr);
+    return console.log(
+        func(arr),
+        );
+
 }
-firstWordUpperCase('The function should console log a step shape')
+
+const callback = (x) => {
+  return x=x.join('-');
+}
+    firstWordUpperCase('The function should console log a step shape',callback);
