abcdc4bd28cba833177480b35a93bbe10cc868f1
Omer Evron <omerevron011@gmail.com>
2022-10-13 12:25:34 +0300

    last

--- DOM/21.5/app.js
+++ DOM/21.5/app.js
@@ -2,16 +2,17 @@ const input = document.querySelector('input')
 const btn = document.querySelector('button')
 const error = document.querySelector('.error')
 const images = document.querySelector('.images')
-btn.addEventListener('click', ()=> {
-    images.innerHTML=''
-    if(Number(input.value)){
-        for(let i=0; i<(Number(input.value)); i++){
+btn.addEventListener('click', () => {
+    images.innerHTML = ''
+    if (Number(input.value)) {
+        for (let i = 0; i < (Number(input.value)); i++) {
             let smiley = createEl()
             document.querySelector('.images').appendChild(smiley)
-        error.style ='display:none;'}
-        } else {
-            error.style = "display:block;"
+            error.style = 'display:none;'
         }
+    } else {
+        error.style = "display:block;"
+    }
 })
 
 function createEl() {
