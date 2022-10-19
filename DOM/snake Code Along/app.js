c9e6dd1427eb572b265c4d694387bf9608ad2329
Omer Evron <omerevron011@gmail.com>
2022-10-19 15:29:07 +0300

    done

---
+++ DOM/snake Code Along/app.js
@@ -0,0 +1,45 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from './snake.js'
+import { update as updateFood, draw as drawFood, } from './food.js'
+import { outsideGrid } from './grid.js'
+
+const gameBoard = document.getElementById('game-board')
+let lastRenderTime = 0
+let gameOver = false
+
+
+function main (currentTime) {
+    if(gameOver){
+        if(confirm('You lost. Press Ok to restart')){
+            window.location = ''
+        } 
+        return
+    } 
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime-lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    console.log('Render');
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update () {
+updateSnake()
+updateFood()
+checkDeath()
+}
+
+function draw () {
+    gameBoard.innerHTML=' '
+drawSnake(gameBoard)
+drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead() || snakeIntersection() )
+}
